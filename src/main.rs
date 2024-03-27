893b3f4eee695295f80eb0a366a4da8d96d6722f
Auggie Bennett <auggie@thebennett.net>
2024-03-27 14:18:52 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+pub struct App {
+    pub blacks: Vec,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64,
+}
+
